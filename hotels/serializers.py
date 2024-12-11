e66e1eb242014ded6765ff6152818c2bf12399a3
Никита Шидогубов <nikita@mer1d1an.ru>
2024-12-11 17:04:46 +0300

    [~] Изменение файла serializers.py R: изменение формата json FB: закрепление полей у Удобств сериализаций, и добавление в каждый сериалайзер Отеля и Номера с полем, которое может содержать несколько значений

--- hotels/serializers.py
+++ hotels/serializers.py
@@ -2,25 +2,29 @@ from rest_framework import serializers
 from .models import Hotel, HotelRoom, AmenityRoom, AmenityHotel
 
 
-class HotelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hotel
-        fields = "__all__"
-
-
-class HotelRoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = HotelRoom
-        fields = "__all__"
-
-
 class AmenityRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = AmenityRoom
+        fields = ["id", "name"]
+
+
+class HotelRoomSerializer(serializers.ModelSerializer):
+    amenities = AmenityRoomSerializer(many=True)
+
+    class Meta:
+        model = HotelRoom
         fields = "__all__"
 
 
 class AmenityHotelSerializer(serializers.ModelSerializer):
     class Meta:
         model = AmenityHotel
+        fields = ["id", "name"]
+
+
+class HotelSerializer(serializers.ModelSerializer):
+    amenities = AmenityHotelSerializer(many=True)
+
+    class Meta:
+        model = Hotel
         fields = "__all__"
