e83d67f563de6af7eb14cf505f791979436570ae
Никита Шидогубов <nikita@mer1d1an.ru>
2024-12-12 14:31:21 +0300

    [+] Добавление ссылок для типов отдыха отеля

--- hotels/urls.py
+++ hotels/urls.py
@@ -18,4 +18,7 @@ urlpatterns = [
     # Удобства в отелях
     path("amenity-hotels/", AmenityHotelListCreateView.as_view(), name="amenity-hotel-list-create",),
     path("amenity-hotels/<int:pk>/", AmenityHotelDetailView.as_view(), name="amenity-hotel-detail",),
+    # Типы отдыха
+    path("places-hotels/", PlaceHotelListCreateView.as_view(), name="place-hotels-list-create",),
+    path("places-hotels/<int:pk>/", PlaceHotelDetailView.as_view(), name="place-hotels-detail",),
 ]
