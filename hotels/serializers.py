921df54d65e69bd89fce84cec80a0df8c39bcf46
Никита Шидогубов <nikita@mer1d1an.ru>
2024-12-12 14:19:03 +0300

    [~] Изменение сериализации R: Не верно передавалась в json FB: поменял местами для визуальной логики, и для необходимых полей предоставил нормальную выгрузку

--- hotels/serializers.py
+++ hotels/serializers.py
@@ -8,18 +8,21 @@ class AmenityRoomSerializer(serializers.ModelSerializer):
         fields = ["id", "name"]
 
 
-class HotelRoomSerializer(serializers.ModelSerializer):
-    amenities = AmenityRoomSerializer(many=True)
-
-    class Meta:
-        model = HotelRoom
-        fields = "__all__"
-
-
 class AmenityHotelSerializer(serializers.ModelSerializer):
     class Meta:
         model = AmenityHotel
         fields = ["id", "name"]
+
+
+class HotelRoomSerializer(serializers.ModelSerializer):
+    amenities = serializers.PrimaryKeyRelatedField(queryset=AmenityRoom.objects.all(), many=True, write_only=True)
+    amenities_room = AmenityRoomSerializer(source="amenities", many=True, read_only=True)
+
+    class Meta:
+
+        model = HotelRoom
+        fields = "__all__"
+
 
 class PlaceHotelSerializer(serializers.ModelSerializer):
     class Meta:
@@ -28,10 +31,11 @@ class PlaceHotelSerializer(serializers.ModelSerializer):
 
 
 class HotelSerializer(serializers.ModelSerializer):
-    amenities = AmenityHotelSerializer(many=True, read_only=True)
-    place = PlaceHotelSerializer(many=True, read_only=True)
+    amenities = serializers.PrimaryKeyRelatedField(queryset=AmenityHotel.objects.all(), many=True, write_only=True)
+    amenities_hotel = AmenityHotelSerializer(source="amenities", many=True, read_only=True)
+    place = serializers.PrimaryKeyRelatedField(queryset=PlaceHotel.objects.all(), many=False, write_only=True)
+    place_hotel = PlaceHotelSerializer(source="place", many=False, read_only=True)
     hotel_room = HotelRoomSerializer(many=True, read_only=True)
-
 
     class Meta:
         model = Hotel
