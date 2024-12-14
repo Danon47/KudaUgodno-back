917a20f3901cbda0b1243d95587d3cf5cf8a1663
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-12-14 11:30:50 +0300

    [+] Создание СRUD для рейса R: Добавление возможности создания, просмотра, редактирования, удаления экземпляров класса Рейс FB: Создание дженериков в views.py, сериализатора для модели Flight в serializers.py

--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+)
 
-# Create your views here.
+from flights.models import Flight
+from flights.serializers import FlightSerializer
+
+
+class FlightCreateAPIView(CreateAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class FlightListAPIView(ListAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class FlightRetrieveAPIView(RetrieveAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class FlightUpdateAPIView(UpdateAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class FlightDestroyAPIView(DestroyAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
