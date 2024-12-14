13d251ef851655a840596a88d670ad87ff05fee3
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-12-14 13:13:57 +0300

    [*] Изменение маршрутов R: Улучшение читаемости маршрутов FB: Изменение файла urls.py
    FB: Регистрация модели рейса в admin.py

--- flights/urls.py
+++ flights/urls.py
@@ -4,19 +4,17 @@ from flights.apps import FlightsConfig
 from flights.views import (
     FlightCreateAPIView,
     FlightRetrieveAPIView,
-    FlightDestroyAPIView, FlightListAPIView,
+    FlightDestroyAPIView,
+    FlightListAPIView,
+    FlightUpdateAPIView,
 )
 
 app_name = FlightsConfig.name
 
 urlpatterns = [
-    path("flights/create/", FlightCreateAPIView.as_view(), name="flight-create"),
-    path("flights/", FlightListAPIView.as_view(), name="flights-list"),
-    path("flights/<int:pk>/", FlightRetrieveAPIView.as_view(), name="flight-detail"),
-    path(
-        "flights/<int:pk>/update/", FlightRetrieveAPIView.as_view(), name="flight-update"
-    ),
-    path(
-        "flights/<int:pk>/delete/", FlightDestroyAPIView.as_view(), name="flight-delete"
-    ),
+    path("create/", FlightCreateAPIView.as_view(), name="flight-create"),
+    path("", FlightListAPIView.as_view(), name="flights-list"),
+    path("<int:pk>/", FlightRetrieveAPIView.as_view(), name="flight-detail"),
+    path("<int:pk>/update/", FlightUpdateAPIView.as_view(), name="flight-update"),
+    path("<int:pk>/delete/", FlightDestroyAPIView.as_view(), name="flight-delete"),
 ]
