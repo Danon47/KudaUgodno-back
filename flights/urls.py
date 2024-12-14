67cd2bc7e24ce84b28a3dbf2e7628a8a7a90c519
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-12-14 11:39:53 +0300

    [+] Создание путей для рейса FB: Добавление путей в urls.py

--- flights/urls.py
+++ flights/urls.py
@@ -1,7 +1,22 @@
+from django.urls import path
+
 from flights.apps import FlightsConfig
+from flights.views import (
+    FlightCreateAPIView,
+    FlightRetrieveAPIView,
+    FlightDestroyAPIView, FlightListAPIView,
+)
 
 app_name = FlightsConfig.name
 
 urlpatterns = [
-
+    path("flights/create/", FlightCreateAPIView.as_view(), name="flight-create"),
+    path("flights/", FlightListAPIView.as_view(), name="flights-list"),
+    path("flights/<int:pk>/", FlightRetrieveAPIView.as_view(), name="flight-detail"),
+    path(
+        "flights/<int:pk>/update/", FlightRetrieveAPIView.as_view(), name="flight-update"
+    ),
+    path(
+        "flights/<int:pk>/delete/", FlightDestroyAPIView.as_view(), name="flight-delete"
+    ),
 ]
