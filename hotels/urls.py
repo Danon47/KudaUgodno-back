1c60ab83072969193749d348f6651d7679f4b43f
Никита Шидогубов <nikita@mer1d1an.ru>
2024-12-14 20:44:59 +0300

    [~] Изменение url.py R: приведение к единому виду FB: изменил отображение и порядок, а так же изменил urls

--- hotels/urls.py
+++ hotels/urls.py
@@ -6,18 +6,16 @@ app_name = HotelsConfig.name
 
 
 urlpatterns = [
-    # Отели
-    path("", HotelListCreateView.as_view(), name="hotel-list-create"),
-    path("<int:pk>/", HotelDetailView.as_view(), name="hotel-detail"),
     # Номера отеля
     path("hotel-rooms/", HotelRoomListCreateView.as_view(), name="hotel-room-list-create"),
     path("hotel-rooms/<int:pk>/", HotelRoomDetailView.as_view(), name="hotel-room-detail"),
     # Удобства в номерах
-    path("amenity-rooms/", AmenityRoomListCreateView.as_view(), name="amenity-room-list-create",),
-    # path("amenity-rooms/<int:pk>/", AmenityRoomDetailView.as_view(), name="amenity-room-detail",),
-    # Удобства в отелях
-    path("amenity-hotels/", AmenityHotelListCreateView.as_view(), name="amenity-hotel-list-create",),
-    # path("amenity-hotels/<int:pk>/", AmenityHotelDetailView.as_view(), name="amenity-hotel-detail",),
+    path("hotel-rooms/amenities/", AmenityRoomListCreateView.as_view(), name="amenity-room-list-create"),
     # Категория номера
-    path("hotel-rooms-categories/", CategoryHotelRoomListCreateView.as_view(), name="hotel-room-categories-list-create",),
+    path("hotel-rooms/categories/", CategoryHotelRoomListCreateView.as_view(),name="hotel-room-categories-list-create"),
+    # Отели
+    path("", HotelListCreateView.as_view(), name="hotel-list-create"),
+    path("<int:pk>/", HotelDetailView.as_view(), name="hotel-detail"),
+    # Удобства в отелях
+    path("amenities/", AmenityHotelListCreateView.as_view(), name="amenity-hotel-list-create"),
 ]
