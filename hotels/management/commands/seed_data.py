85f8499f9b9150eff2addf06e1b068c0cb50f621
Никита Шидогубов <nikita@mer1d1an.ru>
2024-12-15 00:03:19 +0300

    [~] Изменение docker-compose.yaml и добавление файла seed_data R: устал заполнять удобства номера, отеля, категории, и создавать админа FB: изменил docker-compose.yaml добавил команду по заполнению базы данных удобствами отелей и номера, категория номера, и создание администратора. Изменил файл .env.sample добавил туда необходимые поля

---
+++ hotels/management/commands/seed_data.py
@@ -0,0 +1,68 @@
+import os
+
+from django.core.management.base import BaseCommand
+from hotels.models import AmenityHotel, AmenityRoom, CategoryRoom
+from users.models import User
+
+
+class Command(BaseCommand):
+    help = "Заполняет базу данных начальными данными"
+
+    def handle(self, *args, **kwargs):
+        # Данные удобств отелей, номеров, категорий
+        amenities_rooms = [
+            {
+                "name": [
+                    "Душ на этаже",
+                    "Душ в комнате",
+                    "Ванна",
+                    "Дополнительная кровать",
+                    "Фен",
+                    "Сейф",
+                    "Чайный набор",
+                    "Wi-Fi",
+                    "Кондиционер",
+                ],
+            }
+        ]
+        amenities_hotel = [
+            {
+                "name": [
+                    "Бассейн",
+                    "Собственный пляж",
+                    "Семейные номера",
+                    "Детский клуб",
+                    "Аквапарк",
+                    "Теннисный корт",
+                    "Бесплатный интернет",
+                ],
+            }
+        ]
+        category_rooms = [
+            {
+                "name": [
+                    "Стандарт",
+                    "Комфорт",
+                    "Семейный",
+                    "Люкс",
+                ]
+            }
+        ]
+
+        # Заполняем базу данных
+        for amenity_room in amenities_rooms[0]["name"]:
+            AmenityRoom.objects.create(name=amenity_room)
+        for amenity_hotel in amenities_hotel[0]["name"]:
+            AmenityHotel.objects.create(name=amenity_hotel)
+        for category_room in category_rooms[0]["name"]:
+            CategoryRoom.objects.create(name=category_room)
+
+        self.stdout.write(self.style.SUCCESS("Данные успешно заполнены"))
+
+        # Создаем администратора
+        User.objects.create_superuser(
+            username=os.getenv("ADMIN_USERNAME"),
+            password=os.getenv("ADMIN_PASSWORD"),
+        )
+
+        self.stdout.write(self.style.SUCCESS("Администратор успешно создан"))
