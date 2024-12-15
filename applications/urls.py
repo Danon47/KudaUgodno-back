b8260d5c0ce5b6da20fe956b100228fbb347181a
Anton Gromov <seed14112@yandex.ru>
2024-12-15 19:28:31 +0300

    [+] Добавление urls для модели Application (create, list)

--- applications/urls.py
+++ applications/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
+
 from applications.apps import ApplicationsConfig
+from applications.views import ApplicationListCreateView
 
 app_name = ApplicationsConfig.name
 
 urlpatterns = [
-
+    path("", ApplicationListCreateView.as_view(), name="application-list-create"),
 ]
