7dfb34e9305201a32c50ca2d55f9677db115eb8f
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-12-16 17:57:39 +0300

    [+] Регистрация модели Тура в admin.py

--- tours/admin.py
+++ tours/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from tours.models import Tour
+
+
+@admin.register(Tour)
+class TourAdmin(admin.ModelAdmin):
+    list_display = (
+            'id',
+            'name',
+            'start_date',
+            'end_date',
+            'flight_to',
+            'flight_to',
+            'tour_operator',
+            'hotel',
+            'room',
+            'country',
+            'city',
+            'type_of_holiday',
+            'meal_cost',
+            'price'
+        )
+    list_filter = ("tour_operator", "country")
+    search_fields = ("start_date", "hotel")
