bf26468fa2efc7485c7c5772b20749479c6ccd1b
Николай Гордеев <ncgordeev@gmail.com>
2024-12-17 14:27:41 +0300

    [+] Добавлены маршруты для модели пользователя

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,15 @@
+from django.urls import path
+
 from users.apps import UsersConfig
+
+from .views import (UserDestroyView, UserListCreateView, UserRetrieveView,
+                    UserUpdateView)
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-
+    path("users/", UserListCreateView.as_view(), name="user_list_create"),
+    path("users/<int:pk>/", UserRetrieveView.as_view(), name="user_retrieve"),
+    path("users/<int:pk>/update/", UserUpdateView.as_view(), name="user_update"),
+    path("users/<int:pk>/delete/", UserDestroyView.as_view(), name="user_delete"),
 ]
