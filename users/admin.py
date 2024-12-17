621a5ee45f8ef54136cf6d1088be18762f0d64e1
Николай Гордеев <ncgordeev@gmail.com>
2024-12-17 11:56:00 +0300

    [+] Регистрация модели пользователя в админ-панели

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "phone_number", "user_type", "is_active")
+    list_filter = ("user_type", "phone_number")
+    search_fields = ("phone_number", "last_name")
