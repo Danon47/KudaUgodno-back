e73d5045ec074e4a4e4df769f62d788f4f0fff99
Николай Гордеев <ncgordeev@gmail.com>
2024-12-17 11:42:50 +0300

    [+] Добавлена модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,7 +1,20 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 class User(AbstractUser):
     """
     Модель Пользователя
     """
-    pass
+
+    USER_TYPE_CHOICES = (
+        ("admin", "Администратор"),
+        ("tour_operator", "Туроператор"),
+        ("regular_user", "Пользователь"),
+    )
+
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, default="regular_user")
+    phone_number = models.CharField(max_length=15, blank=True, null=True)  # Добавлено поле телефона
+
+    def __str__(self):
+        return self.username
