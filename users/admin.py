fba4b1eec784f3cb80cebc9da453f4f20d578348
Николай Гордеев <ncgordeev@gmail.com>
2024-12-18 14:35:18 +0300

    [*] изменена регистрация в админ-панели. FB: удалено поле user_type

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,17 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "first_name", "last_name", "phone_number", "is_staff", "is_active",)
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "phone_number",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    )
     list_filter = ("phone_number",)
-    search_fields = ("phone_number", "last_name",)
+    search_fields = (
+        "phone_number",
+        "last_name",
+    )
