554b98414529b7f00411861142afb0526929fc1a
Николай Гордеев <ncgordeev@gmail.com>
2024-12-18 14:17:00 +0300

    [*] исправлена модель пользователя.

--- users/models.py
+++ users/models.py
@@ -7,11 +7,17 @@ class User(AbstractUser):
     Модель Пользователя
     """
 
+    username = None
+
     phone_number = PhoneNumberField(
         region="RU",
         verbose_name="Телефон",
+        unique=True,
         help_text="Номер телефона в формате: +7 (999) 999-99-99",
     )  # Добавлено поле телефона
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.username
