9db83fbffe9eb7d05639e0be3402d8d2631dd26a
Николай Гордеев <ncgordeev@gmail.com>
2024-12-19 14:19:43 +0300

    [*] Изменен сериализатор. fb: Удалена валидация поля - phone_number. Добавлено поле avatar в сериализатор.

--- users/serializers.py
+++ users/serializers.py
@@ -16,15 +16,10 @@ class UserSerializer(serializers.ModelSerializer):
             "last_name",
             "password",
             "phone_number",
+            "avatar",
         ]
-        read_only_fields = ("is_stuff", "is_superuser",)
+        read_only_fields = ("is_staff", "is_superuser",)
         extra_kwargs = {"password": {"write_only": True}}
-
-    def validate_phone_number(self, value):
-        """Проверка на уникальность номера телефона."""
-        if User.objects.filter(phone_number=value).exists():
-            raise serializers.ValidationError("Этот номер телефона уже используется.")
-        return value
 
     def create(self, validated_data):
         user = User(**validated_data)
