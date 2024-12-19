e2fc3eb7f759e792af5f44010ce3bd42d44cace4
Николай Гордеев <ncgordeev@gmail.com>
2024-12-19 14:21:38 +0300

    [*] Изменен файл маршрутов. fb: Сокращены urls

--- users/urls.py
+++ users/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 
 from users.apps import UsersConfig
 
-from .views import (UserDestroyView, UserListCreateView, UserRetrieveView,
-                    UserUpdateView)
+from .views import UserDestroyView, UserListCreateView, UserRetrieveView, UserUpdateView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("users/", UserListCreateView.as_view(), name="user_list_create"),
-    path("users/<int:pk>/", UserRetrieveView.as_view(), name="user_retrieve"),
-    path("users/<int:pk>/update/", UserUpdateView.as_view(), name="user_update"),
-    path("users/<int:pk>/delete/", UserDestroyView.as_view(), name="user_delete"),
+    path("/", UserListCreateView.as_view(), name="user_list_create"),
+    path("<int:pk>/", UserRetrieveView.as_view(), name="user_retrieve"),
+    path("<int:pk>/update/", UserUpdateView.as_view(), name="user_update"),
+    path("<int:pk>/delete/", UserDestroyView.as_view(), name="user_delete"),
 ]
