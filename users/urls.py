1f09091902481efdb627cafa4d7040e8fb76aca5
Николай Гордеев <ncgordeev@gmail.com>
2024-12-19 17:17:00 +0300

    [*] Изменены маршруты. fb: Изменен путь для создания пользователя.

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ from .views import UserDestroyView, UserListCreateView, UserRetrieveView, UserUp
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("/", UserListCreateView.as_view(), name="user_list_create"),
+    path("", UserListCreateView.as_view(), name="user_list_create"),
     path("<int:pk>/", UserRetrieveView.as_view(), name="user_retrieve"),
     path("<int:pk>/update/", UserUpdateView.as_view(), name="user_update"),
     path("<int:pk>/delete/", UserDestroyView.as_view(), name="user_delete"),
