395611d72c2b7de94fc5a02b140755e7c15ec395
Николай Гордеев <ncgordeev@gmail.com>
2024-12-19 17:13:52 +0300

    [*] Изменена модель пользователя. fb: Добавлен кастомный менеджер пользователя. Указан в модели.

---
+++ users/managers.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Менеджер для модели Пользователя
+    """
+
+    def create_user(self, phone_number, password=None, **extra_fields):
+        """
+        Создает и возвращает пользователя с номером телефона и паролем.
+        """
+        if not phone_number:
+            raise ValueError("Пользователь должен иметь номер телефона")
+
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        """
+        Создает и возвращает суперпользователя с номером телефона и паролем.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Суперпользователь должен иметь is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Суперпользователь должен иметь is_superuser=True.")
+
+        return self.create_user(phone_number, password, **extra_fields)
