a90015846e880925cf2fc7f7508b1db12d9a6a99
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-12-21 14:29:47 +0300

    [*] Изменение модели Flight R: Необходимо не вводить авиакомпанию, а выбирать, добавить поля, валидацию. FB: Добавление choices в поле airlines, добавление полей service_class, flight_type, создание choices.py

--- flights/models.py
+++ flights/models.py
@@ -1,4 +1,7 @@
+from django.core.validators import RegexValidator
 from django.db import models
+
+from flights.choices import AirlinesChoices, ServicesClassChoices, FlightTypeChoices
 
 
 class Flight(models.Model):
@@ -8,12 +11,23 @@ class Flight(models.Model):
 
     # Номер рейса
     flight_number = models.CharField(
-        max_length=10, verbose_name="Номер рейса", help_text="Введите номер рейса"
+        max_length=10,
+        verbose_name="Номер рейса",
+        help_text="Введите номер рейса",
+        validators=[
+            RegexValidator(
+                regex=r"^[A-Z]{2} [0-9]{4}$",
+                message="Введите номер рейса в формате: AA XXXX, где A-латинские буквы в верхнем регистре, X- цифры "
+                "от 0 до 9)",
+            )
+        ],
     )
+
     # Авиакомпания
     airline = models.CharField(
         max_length=100,
         verbose_name="Авиакомпания",
+        choices=AirlinesChoices.choices,
         help_text="Введите название авиакомпании",
     )
     # Аэропорт вылета
@@ -34,15 +48,18 @@ class Flight(models.Model):
     )
     # Время вылета
     departure_time = models.TimeField(
-        verbose_name="Время вылета", help_text="Укажите время вылета"
+        verbose_name="Время вылета",
+        help_text="Укажите время вылета",
     )
     # Дата прибытия
     arrival_date = models.DateField(
-        verbose_name="Дата прибытия", help_text="Укажите дату прибытия"
+        verbose_name="Дата прибытия",
+        help_text="Укажите дату прибытия",
     )
     # Время прибытия
     arrival_time = models.TimeField(
-        verbose_name="Время прибытия", help_text="Укажите время прибытия"
+        verbose_name="Время прибытия",
+        help_text="Укажите время прибытия",
     )
     # Цена за билет
     price = models.DecimalField(
@@ -50,6 +67,20 @@ class Flight(models.Model):
         decimal_places=2,
         verbose_name="Цена",
         help_text="Укажите цену билета",
+    )
+    # Клас обслуживания
+    service_class = models.CharField(
+        max_length=100,
+        verbose_name="Класс обслуживания",
+        choices=ServicesClassChoices.choices,
+        default=ServicesClassChoices.ECONOMY[0],
+    )
+    # Тип рейса
+    flight_type = models.CharField(
+        max_length=50,
+        verbose_name="Тип рейса",
+        choices=FlightTypeChoices.choices,
+        default=FlightTypeChoices.REGULAR[0],
     )
 
     class Meta:
