b0b3e4ba4b634309ccbe9dcdf8fd9aee08ddac2a
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-12-24 20:57:59 +0300

    [+] Добавление валидации FB: Добавление валидации дат начала и окончания тура

--- tours/serializers.py
+++ tours/serializers.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from tours.models import Tour
@@ -8,19 +11,44 @@ class TourSerializer(ModelSerializer):
     Сериализатор для модели Tour.
     """
 
+    def validate_start_date(self, value):
+        """Проверка, что дата начала тура не в прошлом."""
+        if value < date.today():
+            raise serializers.ValidationError(
+                "Дата начала тура не может быть в прошлом."
+            )
+        return value
+
+    def validate_end_date(self, value):
+        """Проверка, что дата окончания тура позже даты начала."""
+        start_date = self.initial_data.get("start_date")
+        if start_date:
+            if isinstance(start_date, str):
+                start_date = date.fromisoformat(
+                    start_date
+                )  # Преобразование строки в объект date
+
+            if value <= start_date:
+                raise serializers.ValidationError(
+                    "Дата окончания тура должна быть позже даты начала."
+                )
+
+        return value
+
     class Meta:
         model = Tour
         fields = (
-            'id',
-            'start_date',
-            'end_date',
-            'flight_to',
-            'flight_to',
-            'tour_operator',
-            'hotel',
-            'room',
-            'meal_cost',
-            'price'
+            "id",
+            "start_date",
+            "end_date",
+            "flight_to",
+            "flight_from",
+            "departure_city",
+            "guests_number",
+            "tour_operator",
+            "hotel",
+            "room",
+            "meal_cost",
+            "price",
         )
-        read_only_fields = ('price', 'meal_cost')
-
+        read_only_fields = ("price", "meal_cost")
