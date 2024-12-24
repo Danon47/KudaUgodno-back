0ccd5180722884209cebee9c9e624220e69ceae3
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-12-24 21:00:28 +0300

    [*] Корректировка тестов

--- tours/tests.py
+++ tours/tests.py
@@ -1,9 +1,8 @@
+from datetime import date
+
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from hotels.choices import TypeOfHolidayChoices
-from hotels.models import Room, RoomCategory
-from tours.choices import FoodChoices
 from tours.models import Tour
 from django.urls import reverse
 
@@ -15,7 +14,7 @@ class TourTestCase(APITestCase):
 
     def setUp(self):
         self.tour = Tour.objects.create(
-            start_date="2024-08-24", end_date="2024-08-25", departure_city="Москва"
+            start_date="2028-08-24", end_date="2028-08-25", departure_city="Москва"
         )
 
     def test_tour_retrieve(self):
@@ -51,11 +50,22 @@ class TourTestCase(APITestCase):
         Тест проверки изменения тура
         """
         url = reverse("tours:tour_detail", args=(self.tour.pk,))
-        data = {"name": "Тест"}
-        response = self.client.patch(url, data)
+        data = {
+            "start_date": "2029-09-01",
+            "end_date": "2029-09-05",
+            "departure_city": "Санкт-Петербург",
+            "guests_number": 3,
+            "food": "ONLY_BREAKFAST",
+            "price": 12000,
+        }
+        response = self.client.patch(url, data, format="json")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(data.get("name"), "Тест")
+        self.tour.refresh_from_db()
+        self.assertEqual(self.tour.start_date, date(2029, 9, 1))
+        self.assertEqual(self.tour.end_date, date(2029, 9, 5))
+        self.assertEqual(self.tour.departure_city, "Санкт-Петербург")
+        self.assertEqual(self.tour.guests_number, 3)
 
     def test_tour_delete(self):
         """
