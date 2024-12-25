6ce2133ab63aee155741c773db4a02bf6dc4b7b3
Anton Gromov <seed14112@yandex.ru>
2024-12-25 11:06:18 +0300

    [~] рефакторинг валидаторов
    R: Был повторяющийся код и валидвторы в разных местах
    
    FB: убран validate из сериализатора и добавлен валидатор DateValidator для проверки даты. Переписан валидатор AirportValidator на ForbiddenWordValidator и сделан универсальным

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,10 +1,8 @@
-from datetime import datetime
-
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from flights.models import Flight
-from flights.validarors import AirportValidator
+from flights.validarors import DateValidator, ForbiddenWordValidator
 
 
 class FlightSerializer(ModelSerializer):
@@ -12,33 +10,33 @@ class FlightSerializer(ModelSerializer):
     Сериализатор для модели Flight.
     """
 
-    def validate(self, data):
-        """
-        Проверка, что дата и время прибытия позже даты и времени отправления.
-        """
-        departure_date = data.get("departure_date")
-        arrival_date = data.get("arrival_date")
-
-        if not departure_date or not arrival_date:
-            raise serializers.ValidationError("Дата вылета и/или прилета отсутствует.")
-
-        departure_time = data.get("departure_time")
-        arrival_time = data.get("arrival_time")
-
-        if not departure_time or not arrival_time:
-            raise serializers.ValidationError("Время вылета и/или прилета отсутствует.")
-
-        departure_datetime = datetime.combine(departure_date, departure_time)
-        arrival_datetime = datetime.combine(arrival_date, arrival_time)
-
-        if arrival_datetime <= departure_datetime:
-            raise serializers.ValidationError(
-                {
-                    "arrival_date": "Дата и время прилета должны быть позже даты и времени вылета."
-                }
-            )
-
-        return data
+    # def validate(self, data):
+    #     """
+    #     Проверка, что дата и время прибытия позже даты и времени отправления.
+    #     """
+    #     departure_date = data.get("departure_date")
+    #     arrival_date = data.get("arrival_date")
+    #
+    #     if not departure_date or not arrival_date:
+    #         raise serializers.ValidationError("Дата вылета и/или прилета отсутствует.")
+    #
+    #     departure_time = data.get("departure_time")
+    #     arrival_time = data.get("arrival_time")
+    #
+    #     if not departure_time or not arrival_time:
+    #         raise serializers.ValidationError("Время вылета и/или прилета отсутствует.")
+    #
+    #     departure_datetime = datetime.combine(departure_date, departure_time)
+    #     arrival_datetime = datetime.combine(arrival_date, arrival_time)
+    #
+    #     if arrival_datetime <= departure_datetime:
+    #         raise serializers.ValidationError(
+    #             {
+    #                 "arrival_date": "Дата и время прилета должны быть позже даты и времени вылета."
+    #             }
+    #         )
+    #
+    #     return data
 
     class Meta:
         model = Flight
@@ -59,6 +57,11 @@ class FlightSerializer(ModelSerializer):
                 fields=["flight_number", "departure_date"],
                 queryset=Flight.objects.all(),  # Валидатор для проверки уникальности рейса в конкретную дату
             ),
-            AirportValidator(field="departure_airport"),
-            AirportValidator(field="arrival_airport"),
+            ForbiddenWordValidator(fields=["departure_airport", "arrival_airport"]),
+            DateValidator(
+                "departure_date",
+                "departure_time",
+                "arrival_date",
+                "arrival_time",
+            ),
         ]
--- flights/validarors.py
+++ flights/validarors.py
@@ -1,16 +1,42 @@
+from datetime import datetime
+
 from rest_framework.exceptions import ValidationError
 
 
-class AirportValidator:
+class ForbiddenWordValidator:
     """
-    Валидатор на наличие недопустимых слов в названии аэропорта.
+    Валидатор на наличие недопустимых слов в названии.
     """
-    def __init__(self, field):
-        self.field = field
+    def __init__(self, fields: list):
+        self.fields = fields
 
     forbidden_words = ["крипта", "оскорбления", "мат", "реклама"]
 
     def __call__(self, value):
-        tmp_val = dict(value).get(self.field)
-        if tmp_val.lower() in self.forbidden_words:
-            raise ValidationError("Название аэропорта содержит недопустимые слова")
+        for field in self.fields:
+            tmp_val = dict(value).get(field)
+            if tmp_val.lower() in self.forbidden_words:
+                raise ValidationError("Название аэропорта содержит недопустимые слова")
+
+
+class DateValidator:
+    """
+    Валидатор для проверки, что дата и время прибытия позже даты и времени отправления.
+    """
+
+    def  __init__(self, departure_date, departure_time, arrival_date, arrival_time):
+        self.departure_date = departure_date
+        self.departure_time = departure_time
+        self.arrival_date = arrival_date
+        self.arrival_time = arrival_time
+
+    def __call__(self, value):
+        departure_date_field = value.get("departure_date")
+        departure_time_field = value.get("departure_time")
+        arrival_date_field = value.get("arrival_date")
+        arrival_time_field = value.get("arrival_time")
+
+        departure_datetime = datetime.combine(departure_date_field, departure_time_field)
+        arrival_datetime = datetime.combine(arrival_date_field, arrival_time_field)
+        if arrival_datetime <= departure_datetime:
+            raise ValidationError("Дата и время прилета должны быть позже даты и времени вылета.")
