774f64569a9102f6705030627509ecbf29347ad0
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-12-25 16:45:54 +0300

    [*] Изменение валидаторов FB: удален def validate из сериализаторе, изменен ForbiddenWordValidator

--- flights/serializers.py
+++ flights/serializers.py
@@ -10,34 +10,6 @@ class FlightSerializer(ModelSerializer):
     Сериализатор для модели Flight.
     """
 
-    # def validate(self, data):
-    #     """
-    #     Проверка, что дата и время прибытия позже даты и времени отправления.
-    #     """
-    #     departure_date = data.get("departure_date")
-    #     arrival_date = data.get("arrival_date")
-    #
-    #     if not departure_date or not arrival_date:
-    #         raise serializers.ValidationError("Дата вылета и/или прилета отсутствует.")
-    #
-    #     departure_time = data.get("departure_time")
-    #     arrival_time = data.get("arrival_time")
-    #
-    #     if not departure_time or not arrival_time:
-    #         raise serializers.ValidationError("Время вылета и/или прилета отсутствует.")
-    #
-    #     departure_datetime = datetime.combine(departure_date, departure_time)
-    #     arrival_datetime = datetime.combine(arrival_date, arrival_time)
-    #
-    #     if arrival_datetime <= departure_datetime:
-    #         raise serializers.ValidationError(
-    #             {
-    #                 "arrival_date": "Дата и время прилета должны быть позже даты и времени вылета."
-    #             }
-    #         )
-    #
-    #     return data
-
     class Meta:
         model = Flight
         fields = (
--- flights/validarors.py
+++ flights/validarors.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 
 from rest_framework.exceptions import ValidationError
@@ -7,16 +8,26 @@ class ForbiddenWordValidator:
     """
     Валидатор на наличие недопустимых слов в названии.
     """
+
     def __init__(self, fields: list):
         self.fields = fields
+        self.forbidden_words = self.load_forbidden_words()
 
-    forbidden_words = ["крипта", "оскорбления", "мат", "реклама"]
+    def load_forbidden_words(self):
+        """
+        Загружает запрещенные слова из файла forbidden_words.txt.
+        """
+        file_path = os.path.join(os.path.dirname(__file__), "forbidden_words.txt")
+        with open(file_path, "r", encoding="utf-8") as file:
+            return [line.strip().lower() for line in file if line.strip()]
 
     def __call__(self, value):
         for field in self.fields:
             tmp_val = dict(value).get(field)
-            if tmp_val.lower() in self.forbidden_words:
-                raise ValidationError("Название аэропорта содержит недопустимые слова")
+            if tmp_val and any(
+                word in tmp_val.lower() for word in self.forbidden_words
+            ):
+                raise ValidationError("Введено недопустимое слово")
 
 
 class DateValidator:
@@ -24,7 +35,7 @@ class DateValidator:
     Валидатор для проверки, что дата и время прибытия позже даты и времени отправления.
     """
 
-    def  __init__(self, departure_date, departure_time, arrival_date, arrival_time):
+    def __init__(self, departure_date, departure_time, arrival_date, arrival_time):
         self.departure_date = departure_date
         self.departure_time = departure_time
         self.arrival_date = arrival_date
@@ -36,7 +47,11 @@ class DateValidator:
         arrival_date_field = value.get("arrival_date")
         arrival_time_field = value.get("arrival_time")
 
-        departure_datetime = datetime.combine(departure_date_field, departure_time_field)
+        departure_datetime = datetime.combine(
+            departure_date_field, departure_time_field
+        )
         arrival_datetime = datetime.combine(arrival_date_field, arrival_time_field)
         if arrival_datetime <= departure_datetime:
-            raise ValidationError("Дата и время прилета должны быть позже даты и времени вылета.")
+            raise ValidationError(
+                "Дата и время прилета должны быть позже даты и времени вылета."
+            )
