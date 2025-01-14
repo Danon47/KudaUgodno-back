9e2f5f30596c2ee040ba94c81295ce94ab0e97cc
Никита Шидогубов <mered1an@live.com>
2025-01-14 16:33:36 +0300

    [~] изменение тестов моделей R: были не все поля проверены и не все модели FB: дописал тесты всех полей и всех моделей

--- hotels/tests/tests_models.py
+++ hotels/tests/tests_models.py
@@ -4,9 +4,12 @@ from hotels.choices import MealChoices, TypeOfHolidayChoices, PlaceChoices
 from hotels.models.models_hotel import Hotel
 from hotels.models.models_hotel_amenity import HotelAmenity
 from hotels.models.models_hotel_meal import MealPlan
+from hotels.models.models_hotel_photo import HotelPhoto
 from hotels.models.models_room import Room
 from hotels.models.models_room_amenity import RoomAmenity
 from hotels.models.models_room_caterogy import RoomCategory
+from hotels.models.models_room_photo import RoomPhoto
+from hotels.tests.fixtures.temp_image import create_test_image
 
 
 class ModelTestCase(TestCase):
@@ -41,8 +44,13 @@ class ModelTestCase(TestCase):
             check_out_time=time(11, 0),
         )
         self.hotel.amenities.add(self.amenity_hotel)
+        self.hotel_photo = HotelPhoto.objects.create(
+            hotel=self.hotel,  # Указываем, к какому отелю относится фотография
+            photo=create_test_image(),
+        )
+
         # Создаём объект номера в отеле и связываем его с объектом отеля
-        self.hotel_room = Room.objects.create(
+        self.room = Room.objects.create(
             category=self.category,
             smoking=False,
             area=20,
@@ -52,8 +60,12 @@ class ModelTestCase(TestCase):
             nightly_price=5000,
             hotel=self.hotel,
         )
-        self.hotel_room.amenities.add(self.amenity_room)
-        self.hotel_room.meal.set(self.meal_plans)
+        self.room.amenities.add(self.amenity_room)
+        self.room.meal.set(self.meal_plans)
+        self.room_photo = RoomPhoto.objects.create(
+            room=self.room,  # Указываем, к какому отелю относится фотография
+            photo=create_test_image(),
+        )
 
     def test_hotel_creation(self):
         self.assertTrue(isinstance(self.hotel, Hotel))
@@ -72,20 +84,68 @@ class ModelTestCase(TestCase):
         self.assertEqual(self.hotel.check_in_time, time(15, 0))
         self.assertEqual(self.hotel.check_out_time, time(11, 0))
         # Проверяем, что номер связан с отелем
-        self.assertEqual(self.hotel_room.hotel, self.hotel)
+        self.assertEqual(self.room.hotel, self.hotel)
 
     def test_hotel_room_creation(self):
-        self.assertTrue(isinstance(self.hotel_room, Room))
-        self.assertEqual(self.hotel_room.amenities.first().name, "Кондиционер")
-        self.assertEqual(self.hotel_room.category.name, "Стандарт")
+        self.assertTrue(isinstance(self.room, Room))
+        self.assertEqual(self.room.amenities.first().name, "Кондиционер")
+        self.assertEqual(self.room.category.name, "Стандарт")
         # Проверяем количество типов питания
-        self.assertEqual(self.hotel_room.meal.count(), 6)
+        self.assertEqual(self.room.meal.count(), 6)
         # Проверяем каждый тип питания
         for meal_plan in self.meal_plans:
-            self.assertIn(meal_plan, self.hotel_room.meal.all())
-        self.assertEqual(self.hotel_room.smoking, False)
-        self.assertEqual(self.hotel_room.area, 20)
-        self.assertEqual(self.hotel_room.capacity, 3)
-        self.assertEqual(self.hotel_room.single_bed, 1)
-        self.assertEqual(self.hotel_room.double_bed, 1)
-        self.assertEqual(self.hotel_room.nightly_price, 5000)
+            self.assertIn(meal_plan, self.room.meal.all())
+        self.assertEqual(self.room.smoking, False)
+        self.assertEqual(self.room.area, 20)
+        self.assertEqual(self.room.capacity, 3)
+        self.assertEqual(self.room.single_bed, 1)
+        self.assertEqual(self.room.double_bed, 1)
+        self.assertEqual(self.room.nightly_price, 5000)
+
+    def test_photo_room_creation(self):
+        """Тест проверки создания фотографии отеля"""
+        self.assertTrue(isinstance(self.hotel_photo, HotelPhoto))
+        self.assertEqual(self.hotel_photo.hotel, self.hotel)
+        self.assertTrue(self.hotel_photo.photo)
+
+    def test_photo_hotel_creation(self):
+        """Тест проверки создания фотографии отеля"""
+        self.assertTrue(isinstance(self.room_photo, RoomPhoto))
+        self.assertEqual(self.room_photo.room, self.room)
+        self.assertTrue(self.room_photo.photo)
+
+    def test_hotel_amenity_creation(self):
+        """Тест проверки создания удобства отеля"""
+        self.assertTrue(isinstance(self.amenity_hotel, HotelAmenity))
+        self.assertEqual(self.amenity_hotel.name, "Бассейн")
+
+    def test_room_amenity_creation(self):
+        """Тест проверки создания удобства номера"""
+        self.assertTrue(isinstance(self.amenity_room, RoomAmenity))
+        self.assertEqual(self.amenity_room.name, "Кондиционер")
+
+    def test_room_category_creation(self):
+        """Тест проверки создания категории номера"""
+        self.assertTrue(isinstance(self.category, RoomCategory))
+        self.assertEqual(self.category.name, "Стандарт")
+
+    def test_hotel_meal_creation(self):
+        """Тест проверки создания типа питания"""
+        # Проверяем количество созданных типов питания
+        self.assertEqual(len(self.meal_plans), 6)
+        # Проверяем каждый тип питания по отдельности
+        meal_plan_no_meals = MealPlan.objects.get(name=MealChoices.NO_MEALS)
+        self.assertEqual(meal_plan_no_meals.price_per_person, 0)
+        meal_plan_ultra_all_inclusive = MealPlan.objects.get(name=MealChoices.ULTRA_ALL_INCLUSIVE)
+        self.assertEqual(meal_plan_ultra_all_inclusive.price_per_person, 1000)
+        meal_plan_all_inclusive = MealPlan.objects.get(name=MealChoices.ALL_INCLUSIVE)
+        self.assertEqual(meal_plan_all_inclusive.price_per_person, 750)
+        meal_plan_full_board = MealPlan.objects.get(name=MealChoices.FULL_BOARD)
+        self.assertEqual(meal_plan_full_board.price_per_person, 500)
+        meal_plan_half_board = MealPlan.objects.get(name=MealChoices.HALF_BOARD)
+        self.assertEqual(meal_plan_half_board.price_per_person, 250)
+        meal_plan_only_breakfast = MealPlan.objects.get(name=MealChoices.ONLY_BREAKFAST)
+        self.assertEqual(meal_plan_only_breakfast.price_per_person, 100)
+        # Также можно проверить, что все типы питания связаны с отелем через номер
+        for meal_plan in self.meal_plans:
+            self.assertIn(meal_plan, self.room.meal.all())
