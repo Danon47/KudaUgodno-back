e7516369e3b1a4daeae942ebbc4bbeedd35eab28
Никита Шидогубов <mered1an@live.com>
2025-01-14 12:28:49 +0300

    [~] Изменение urls.py R: заметил только сейчас, что не верно прописано добавление удобств, категорий и фотографий FB: поправил urlы, иначе не верно выглядит

--- hotels/urls.py
+++ hotels/urls.py
@@ -23,16 +23,16 @@ urlpatterns = [
     path("", HotelListCreateAPIView.as_view(), name="hotel-list-create"),
     path("<int:pk>/", HotelDetailAPIView.as_view(), name="hotel-detail-update-delete"),
     # Удобства в отелях
-    path("amenities/", HotelAmenityCreateAPIView.as_view(), name="hotel-amenity-create"),
+    path("<int:pk>/amenities/", HotelAmenityCreateAPIView.as_view(), name="hotel-amenity-create"),
     # Добавление фотографий отеля
-    path("photos/", HotelPhotoCreateAPIView.as_view(), name="hotel-photo-create"),
+    path("<int:pk>/photos/", HotelPhotoCreateAPIView.as_view(), name="hotel-photo-create"),
     # Номера отеля
     path("rooms/", RoomListCreateAPIView.as_view(), name="room-list-create"),
     path("rooms/<int:pk>/", RoomDetailAPIView.as_view(), name="room-detail-update-delete"),
     # Удобства в номерах
-    path("rooms/amenities/", RoomAmenityCreateAPIView.as_view(), name="room-amenity-create"),
+    path("rooms/<int:pk>/amenities/", RoomAmenityCreateAPIView.as_view(), name="room-amenity-create"),
     # Добавление фотографий номера
-    path("rooms/photo/", RoomPhotoCreateAPIView.as_view(), name="room-photo-create"),
+    path("rooms/<int:pk>/photo/", RoomPhotoCreateAPIView.as_view(), name="room-photo-create"),
     # Категория номера
-    path("rooms/categories/", RoomCategoryCreateAPIView.as_view(), name="room-categories-create"),
+    path("rooms/<int:pk>/categories/", RoomCategoryCreateAPIView.as_view(), name="room-categories-create"),
 ]
