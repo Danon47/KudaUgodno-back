1c056eae17bda0c8fe8609aa2f19da7b0547e073
Anton Gromov <seed14112@yandex.ru>
2025-01-30 18:00:50 +0300

    [~] Заменен urls
    R: не нужны стандартные urls для CRUD при viewsets
    
    FB: Убраны urls добавлен router

--- applications/urls.py
+++ applications/urls.py
@@ -1,16 +1,17 @@
+from rest_framework.routers import DefaultRouter
 from django.urls import path
 
 from applications.apps import ApplicationsConfig
-from applications.views.views_application import ApplicationListCreateView, ApplicationDetailView
+from applications.views.views_application import ApplicationViewSet
 from applications.views.views_guest import GuestListCreateView, GuestDetailView
 
 app_name = ApplicationsConfig.name
 
+router = DefaultRouter()
+router.register("", ApplicationViewSet)
+
 urlpatterns = [
-    # Заявка
-    path("", ApplicationListCreateView.as_view(), name="application_list_create"),
-    path("<int:pk>", ApplicationDetailView.as_view(), name="application_detail_update_delete"),
     # Гости
     path("guests/", GuestListCreateView.as_view(), name="guest_list_create"),
     path("guests/<int:pk>", GuestDetailView.as_view(), name="guest_detail_update_delete"),
-]
+] + router.urls
