3720050838c0718a95542a5aa2145bed60cc1ac4
Никита Шидогубов <mered1an@live.com>
2025-01-31 14:58:57 +0300

    [~] изменение главного urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,34 +1,22 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
 from config import settings
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="API бронирования туров или отелей",
-        default_version="v0.3",
-        description='API сервис для веб-сайта "Куда Угодно"',
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json",),
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui",),
-    path("users/", include("users.urls", namespace="users")),
-    path("tours/", include("tours.urls", namespace="tours")),
-    path("flights/", include("flights.urls", namespace="flights")),
-    path("hotels/", include("hotels.urls", namespace="hotels")),
-    path("applications/", include("applications.urls", namespace="applications")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("users/", include("users.urls")),
+    path("tours/", include("tours.urls")),
+    path("flights/", include("flights.urls")),
+    path("", include("hotels.urls.urls_hotel")),
+    path("", include("hotels.urls.urls_room")),
+    path("applications/", include("applications.urls")),
 ]
 
 if settings.DEBUG:
