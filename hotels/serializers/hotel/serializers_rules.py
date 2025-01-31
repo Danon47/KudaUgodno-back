ecfdb019c85c48bedd0b2395bdbeffaa213bee68
Никита Шидогубов <mered1an@live.com>
2025-01-31 14:41:00 +0300

    [~] переписывание отеля для фронта

---
+++ hotels/serializers/hotel/serializers_rules.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from hotels.models.hotel.models_hotel import HotelRules
+
+
+class HotelRulesSerializer(serializers.ModelSerializer):
+    """Сериализатор правил в отеле"""
+
+    class Meta:
+        model = HotelRules
+        fields = ("name", "description",)
