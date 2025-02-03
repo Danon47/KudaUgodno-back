877d3ef4d8cbb34ce955667aac12e1b1944ea79c
Tatyana Kharlamova <kharlamova2785@gmail.com>
2025-02-03 15:45:28 +0300

    [*]Исправление parameters_tour, удаление нумерации

--- flights/views.py
+++ flights/views.py
@@ -1,14 +1,3 @@
-# from rest_framework.generics import (
-#     ListCreateAPIView,
-#     RetrieveUpdateDestroyAPIView,
-# )
-# from drf_yasg import openapi
-# from drf_yasg.utils import swagger_auto_schema
-#
-#
-# from flights.models import Flight
-# from flights.serializers import FlightSerializer
-
 from drf_spectacular.utils import (
     extend_schema,
     extend_schema_view,
@@ -20,7 +9,7 @@ from rest_framework import viewsets
 from flights.models import Flight
 from flights.serializers import FlightSerializer
 
-parameters_application = [
+parameters_tour = [
     OpenApiParameter(
         location=OpenApiParameter.PATH,
         name="id",
@@ -30,7 +19,7 @@ parameters_application = [
     ),
 ]
 
-tags_flight = ["4. Рейсы"]
+tags_flight = ["Рейсы"]
 
 
 @extend_schema_view(
@@ -71,7 +60,7 @@ tags_flight = ["4. Рейсы"]
         summary="Информация о рейсе",
         description="Получение информации о рейсе через идентификатор",
         tags=tags_flight,
-        parameters=parameters_application,
+        parameters=parameters_tour,
         responses={
             200: FlightSerializer,
             404: OpenApiResponse(description="Заявка не найдена"),
@@ -82,7 +71,7 @@ tags_flight = ["4. Рейсы"]
         description="Обновление всех полей рейса",
         request=FlightSerializer,
         tags=tags_flight,
-        parameters=parameters_application,
+        parameters=parameters_tour,
         responses={
             200: FlightSerializer,
             400: OpenApiResponse(description="Ошибка валидации"),
@@ -94,7 +83,7 @@ tags_flight = ["4. Рейсы"]
         description="Обновление отдельных полей рейса",
         request=FlightSerializer,
         tags=tags_flight,
-        parameters=parameters_application,
+        parameters=parameters_tour,
         responses={
             200: FlightSerializer,
             400: OpenApiResponse(description="Ошибка валидации"),
@@ -105,7 +94,7 @@ tags_flight = ["4. Рейсы"]
         summary="Удаление рейса",
         description="Полное удаление рейса",
         tags=tags_flight,
-        parameters=parameters_application,
+        parameters=parameters_tour,
         responses={
             204: OpenApiResponse(description="Рейса удален"),
             404: OpenApiResponse(description="Рейс не найден"),
@@ -115,150 +104,3 @@ tags_flight = ["4. Рейсы"]
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
-
-
-
-# class FlightListCreateView(ListCreateAPIView):
-#     queryset = Flight.objects.all()
-#     serializer_class = FlightSerializer
-#
-#     @swagger_auto_schema(
-#         operation_description="Получение списка рейсов",
-#         operation_summary="Список рейсов",
-#         tags=["4. Рейс"],
-#         manual_parameters=[
-#             openapi.Parameter(
-#                 name="limit",
-#                 in_=openapi.IN_QUERY,
-#                 type=openapi.TYPE_INTEGER,
-#                 description="Количество рейсов для возврата на страницу",
-#             ),
-#             openapi.Parameter(
-#                 name="offset",
-#                 in_=openapi.IN_QUERY,
-#                 type=openapi.TYPE_INTEGER,
-#                 description="Начальный индекс, из которого возвращаются результаты",
-#             ),
-#         ],
-#         responses={
-#             200: openapi.Response(
-#                 description="Успешное получение списка рейсов",
-#                 schema=FlightSerializer(many=True),
-#             ),
-#             400: "Ошибка запроса",
-#         },
-#     )
-#     def get(self, request, *args, **kwargs):
-#         return super().get(request, *args, **kwargs)
-#
-#     @swagger_auto_schema(
-#         operation_description="Создание рейса",
-#         operation_summary="Добавление рейса",
-#         request_body=FlightSerializer,
-#         tags=["4. Рейс"],
-#         responses={
-#             201: openapi.Response(
-#                 description="Успешное создание номера", schema=FlightSerializer()
-#             ),
-#             400: "Ошибка валидации",
-#         },
-#     )
-#     def post(self, request, *args, **kwargs):
-#         return super().post(request, *args, **kwargs)
-#
-#
-# class FlightDetailView(RetrieveUpdateDestroyAPIView):
-#     queryset = Flight.objects.all()
-#     serializer_class = FlightSerializer
-#
-#     @swagger_auto_schema(
-#         operation_summary="Получение детальной информации о рейсе",
-#         operation_description="Возвращает полную информацию о рейсе по его идентификатору",
-#         tags=["4. Рейс"],
-#         manual_parameters=[
-#             openapi.Parameter(
-#                 name="id",
-#                 in_=openapi.IN_PATH,
-#                 type=openapi.TYPE_INTEGER,
-#                 description="Уникальный идентификатор рейса в базе данных",
-#                 required=True,
-#             )
-#         ],
-#         responses={
-#             200: openapi.Response(
-#                 description="Успешное получение информации о рейсе",
-#                 schema=FlightSerializer(),
-#             ),
-#             404: "Рейс не найден",
-#         },
-#     )
-#     def get(self, request, *args, **kwargs):
-#         return super().get(request, *args, **kwargs)
-#
-#     @swagger_auto_schema(
-#         operation_summary="Полное обновление информации о рейсе",
-#         operation_description="Обновляет все поля рейса",
-#         tags=["4. Рейс"],
-#         request_body=FlightSerializer,
-#         manual_parameters=[
-#             openapi.Parameter(
-#                 name="id",
-#                 in_=openapi.IN_PATH,
-#                 type=openapi.TYPE_INTEGER,
-#                 description="Уникальный идентификатор номера в базе данных",
-#                 required=True,
-#             )
-#         ],
-#         responses={
-#             200: openapi.Response(
-#                 description="Рейс успешно обновлен", schema=FlightSerializer()
-#             ),
-#             400: "Ошибка валидации",
-#             404: "Рейс не найден",
-#         },
-#     )
-#     def put(self, request, *args, **kwargs):
-#         return super().put(request, *args, **kwargs)
-#
-#     @swagger_auto_schema(
-#         operation_summary="Частичное обновление информации о рейсе",
-#         operation_description="Обновляет указанные поля рейса",
-#         tags=["4. Рейс"],
-#         request_body=FlightSerializer,
-#         manual_parameters=[
-#             openapi.Parameter(
-#                 name="id",
-#                 in_=openapi.IN_PATH,
-#                 type=openapi.TYPE_INTEGER,
-#                 description="Уникальный идентификатор номера в базе данных",
-#                 required=True,
-#             )
-#         ],
-#         responses={
-#             200: openapi.Response(
-#                 description="Рейс успешно обновлен", schema=FlightSerializer()
-#             ),
-#             400: "Ошибка валидации",
-#             404: "Рейс не найден",
-#         },
-#     )
-#     def patch(self, request, *args, **kwargs):
-#         return super().patch(request, *args, **kwargs)
-#
-#     @swagger_auto_schema(
-#         operation_summary="Удаление рейса",
-#         operation_description="Полное удаление рейса по его идентификатору",
-#         tags=["4. Рейс"],
-#         manual_parameters=[
-#             openapi.Parameter(
-#                 name="id",
-#                 in_=openapi.IN_PATH,
-#                 type=openapi.TYPE_INTEGER,
-#                 description="Уникальный идентификатор рейса",
-#                 required=True,
-#             )
-#         ],
-#         responses={204: "Рейс успешно удален", 404: "Рейс не найден"},
-#     )
-#     def delete(self, request, *args, **kwargs):
-#         return super().delete(request, *args, **kwargs)
