c71d6062a3acc9b2a3152bcbd8431f2a8f2c10fa
Никита Шидогубов <mered1an@live.com>
2025-02-07 11:52:04 +0300

    [~] Обновление модели Номера R: с фронтом начали определяться с полями FB: мало изменений и тестов

--- hotels/models/room/models_room_amenity.py
+++ hotels/models/room/models_room_amenity.py
@@ -1,4 +1,6 @@
 from django.db import models
+from hotels.models.hotel.models_hotel import NULLABLE
+from hotels.models.room.models_room import Room
 
 
 class RoomAmenity(models.Model):
@@ -6,6 +8,14 @@ class RoomAmenity(models.Model):
     Удобства в номере
     """
 
+    room = models.ForeignKey(
+        Room,
+        on_delete=models.CASCADE,
+        related_name="rooms_amenities",
+        verbose_name="Отель",
+        help_text="Отель",
+        **NULLABLE,
+    )
     name = models.CharField(
         max_length=50,
         verbose_name="Удобство",
