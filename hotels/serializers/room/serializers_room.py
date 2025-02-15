cba952f67e72f5267c48e41736b17ec8c1462d0c
Никита Шидогубов <mered1an@live.com>
2025-02-15 22:28:09 +0300

    [~] Изменение названия поля R: забыл переименовть в сериализации FB: из-за этого в документации получаю ошибку 500

--- hotels/serializers/room/serializers_room.py
+++ hotels/serializers/room/serializers_room.py
@@ -34,7 +34,7 @@ class RoomBaseSerializer(serializers.ModelSerializer):
             "id",
             "category",
             "price",
-            "type_of_meal",
+            "type_of_meals",
             "number_of_adults",
             "number_of_children",
             "single_bed",
