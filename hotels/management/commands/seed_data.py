a6411f023eb74fc8623a56527ba4800988ff67f7
Никита Шидогубов <mered1an@live.com>
2025-02-17 09:27:43 +0300

    [-] Удалён лишний код

--- hotels/management/commands/seed_data.py
+++ hotels/management/commands/seed_data.py
@@ -1,6 +1,5 @@
 import os
 from django.core.management.base import BaseCommand
-# from hotels.models.room.models_room_category import RoomCategory
 from users.models import User
 
 
@@ -8,23 +7,6 @@ class Command(BaseCommand):
     help = "Заполняет базу данных начальными данными"
 
     def handle(self, *args, **kwargs):
-        # # Данные удобств отелей, номеров, категорий
-        # category_rooms = [
-        #     {"name": "Стандарт"},
-        #     {"name": "Комфорт"},
-        #     {"name": "Семейный"},
-        #     {"name": "Люкс"},
-        # ]
-        #
-        # # Проверяем, заполнены ли уже данные
-        # if RoomCategory.objects.exists():
-        #     self.stdout.write(self.style.WARNING("=== BD already has these values ==="))
-        # else:
-        #     for category in category_rooms:
-        #         RoomCategory.objects.get_or_create(**category)
-        #
-        #     self.stdout.write(self.style.SUCCESS("=== BD DONE ==="))
-
         # Создание администратора
         email = os.getenv("ADMIN_EMAIL")
         password = os.getenv("ADMIN_PASSWORD")
