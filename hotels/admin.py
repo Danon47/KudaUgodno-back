2e57fb1f2a0d741b4348746d551649056a4338cd
Никита Шидогубов <mered1an@live.com>
2025-02-17 08:57:55 +0300

    [-] Удаление лишнего кода

--- hotels/admin.py
+++ hotels/admin.py
@@ -1,21 +1,8 @@
 from django.contrib import admin
 from hotels.models.hotel.models_hotel import Hotel
-from hotels.models.hotel.models_hotel_amenity import (
-    HotelAmenityCommon,
-    HotelAmenityInTheRoom,
-    HotelAmenitySportsAndRecreation,
-    HotelAmenityForChildren,
-)
 from hotels.models.hotel.models_hotel_photo import HotelPhoto
 from hotels.models.hotel.models_hotel_rules import HotelRules
 from hotels.models.room.models_room import Room
-from hotels.models.room.models_room_amenity import (
-    RoomAmenityCommon,
-    RoomAmenityCoffeeStation,
-    RoomAmenityBathroom,
-    RoomAmenityView,
-)
-# from hotels.models.room.models_room_category import RoomCategory
 from hotels.models.room.models_room_discount import RoomDiscount
 from hotels.models.room.models_room_photo import RoomPhoto
 from hotels.models.room.models_room_unavailable import RoomUnavailable
@@ -36,51 +23,6 @@ class RoomAdmin(admin.ModelAdmin):
         "price",
     )
     list_display_links = ("id", "category")
-
-
-# @admin.register(HotelAmenityCommon)
-# class HotelAmenityCommonAdmin(admin.ModelAdmin):
-#     list_display = ("id", "name")
-#
-#
-# @admin.register(HotelAmenityInTheRoom)
-# class HotelAmenityInTheRoomAdmin(admin.ModelAdmin):
-#     list_display = ("id", "name")
-#
-#
-# @admin.register(HotelAmenitySportsAndRecreation)
-# class HotelAmenitySportsAndRecreationAdmin(admin.ModelAdmin):
-#     list_display = ("id", "name")
-#
-#
-# @admin.register(HotelAmenityForChildren)
-# class HotelAmenityForChildrenAdmin(admin.ModelAdmin):
-#     list_display = ("id", "name")
-
-
-# @admin.register(RoomAmenityCommon)
-# class RoomAmenityAdmin(admin.ModelAdmin):
-#     list_display = ("id", "name")
-#
-#
-# @admin.register(RoomAmenityCoffeeStation)
-# class RoomAmenityAdmin(admin.ModelAdmin):
-#     list_display = ("id", "name")
-#
-#
-# @admin.register(RoomAmenityBathroom)
-# class RoomAmenityAdmin(admin.ModelAdmin):
-#     list_display = ("id", "name")
-#
-#
-# @admin.register(RoomAmenityView)
-# class RoomAmenityAdmin(admin.ModelAdmin):
-#     list_display = ("id", "name")
-
-
-# @admin.register(RoomCategory)
-# class RoomCategoryAdmin(admin.ModelAdmin):
-#     list_display = ("id", "name")
 
 
 @admin.register(RoomPhoto)
