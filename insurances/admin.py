8dcb9934a4b32751132701f252ced118aa38bf28
Никита Шидогубов <mered1an@live.com>
2025-04-10 18:23:05 +0300

    [-] Удаление лишнего кода

--- insurances/admin.py
+++ insurances/admin.py
@@ -1,18 +1,8 @@
 from django.contrib import admin
 
-from insurances.models import InsuranceMedical, InsuranceNotLeaving, Insurances
+from insurances.models import Insurances
 
 
 @admin.register(Insurances)
 class InsuranceAdmin(admin.ModelAdmin):
     list_display = ("id", "medical", "not_leaving")
-
-
-@admin.register(InsuranceMedical)
-class InsuranceMedicalAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
-
-
-@admin.register(InsuranceNotLeaving)
-class InsuranceNotLeavingAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
