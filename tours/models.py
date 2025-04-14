da4d77c5c8d6d481629e2cf10cbef62fc209cd49
Tatyana Kharlamova <kharlamova2785@gmail.com>
2025-04-14 14:29:10 +0300

    [~] Добавление help-текста к полям тура

--- tours/models.py
+++ tours/models.py
@@ -11,39 +11,45 @@ class Tour(models.Model):
     Модель для хранения информации о турах
     """
 
-    start_date = models.DateField(verbose_name="Дата начала тура")
-    end_date = models.DateField(verbose_name="Дата окончания тура")
+    start_date = models.DateField(verbose_name="Дата начала тура", help_text="Введите дату начала тура")
+    end_date = models.DateField(verbose_name="Дата окончания тура", help_text="Введите дату окончания тура")
     flight_to = models.ForeignKey(
         Flight,
         on_delete=models.SET_NULL,
         verbose_name="Рейс отправления",
         related_name="tours",
+        help_text="Введите ID рейса отправления",
         **NULLABLE,
     )
     flight_from = models.ForeignKey(
         Flight,
         on_delete=models.SET_NULL,
         verbose_name="Рейс возвращения",
+        help_text="Введите ID рейса возвращения",
         **NULLABLE,
     )
     departure_country = models.CharField(
         max_length=50,
         verbose_name="Страна вылета",
+        help_text="Введите страну вылета",
         **NULLABLE,
     )
     departure_city = models.CharField(
         max_length=50,
         verbose_name="Город вылета",
+        help_text="Введите город вылеты",
         **NULLABLE,
     )
     arrival_country = models.CharField(
         max_length=50,
         verbose_name="Страна прибытия",
+        help_text="Страну прибытия",
         **NULLABLE,
     )
     arrival_city = models.CharField(
         max_length=50,
         verbose_name="Город прибытия",
+        help_text="Город прибытия",
         **NULLABLE,
     )
     tour_operator = models.ForeignKey(
@@ -51,6 +57,7 @@ class Tour(models.Model):
         on_delete=models.SET_NULL,
         verbose_name="Туроператор",
         related_name="tours",
+        help_text="Введите ID туроператора",
         **NULLABLE,
     )
     hotel = models.ForeignKey(
@@ -58,22 +65,27 @@ class Tour(models.Model):
         on_delete=models.SET_NULL,
         verbose_name="Отель",
         related_name="tours",
+        help_text="Введите ID отеля",
         **NULLABLE,
     )
     room = models.CharField(
         max_length=50,
         verbose_name="Категория номера",
         default="Standard",
+        help_text="Введите категорию номера",
     )
-    transfer = models.BooleanField(verbose_name="Трансфер", default=False)
+    transfer = models.BooleanField(verbose_name="Трансфер", default=False, help_text="Отметьте наличие трансфера")
     price = models.DecimalField(
         max_digits=10,
         decimal_places=2,
         verbose_name="Стоимость тура",
+        help_text="Введите стоимость тура",
         **NULLABLE,
     )
 
-    document = models.FileField(upload_to="tour/documents", verbose_name="Документы", **NULLABLE)
+    document = models.FileField(
+        upload_to="tour/documents", verbose_name="Документы", help_text="Загрузите документы по туру", **NULLABLE
+    )
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name="Дата создания",
