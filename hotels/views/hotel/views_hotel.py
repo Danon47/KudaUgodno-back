942a62a1289bd596243fb1976367a14f9a9328c7
Никита Шидогубов <mered1an@live.com>
2025-04-18 18:18:18 +0300

    [+] Добавление ссылки на вьюху по показу всех отелей для что насчёт погреться. Выводится список отелей которые находятся в тёплых местах, и где есть море, дополнительно проверяется на город, если он в тёплом местое, в этой стране и рядом с морем.

--- hotels/views/hotel/views_hotel.py
+++ hotels/views/hotel/views_hotel.py
@@ -1,10 +1,13 @@
+from random import choice
+
 from drf_spectacular.utils import OpenApiResponse, extend_schema, extend_schema_view
 from rest_framework import viewsets
 
-from all_fixture.fixture_views import hotel_settings, id_hotel, limit, offset
+from all_fixture.fixture_views import hotel_settings, id_hotel, limit, offset, warm_up_settings
 from all_fixture.pagination import CustomLOPagination
 from hotels.models.hotel.models_hotel import Hotel
 from hotels.serializers.hotel.serializers_hotel import HotelBaseSerializer, HotelDetailSerializer, HotelListSerializer
+from hotels.serializers.warm_up.serializers_hotel_warm_up import HotelWarmUpSerializer
 
 
 class CreatedByUserFilterMixin:
@@ -98,6 +101,35 @@ class HotelViewSet(viewsets.ModelViewSet):
             return HotelListSerializer
         else:
             return HotelDetailSerializer
+
+
+@extend_schema_view(
+    list=extend_schema(
+        summary="Список что на счёт погреться",
+        description="Получение списка трёх  отелей в определённом городе",
+        parameters=[offset, limit],
+        responses={
+            200: HotelWarmUpSerializer(many=True),
+            400: OpenApiResponse(description="Ошибка запроса"),
+        },
+        tags=[warm_up_settings["name"]],
+    )
+)
+class HotelWarpUpViewSet(viewsets.ModelViewSet):
+    serializer_class = HotelWarmUpSerializer
+    pagination_class = CustomLOPagination
+
+    def get_queryset(self):
+        # Получаем все отели с warm=True
+        warm_hotels = Hotel.objects.filter(warm=True)
+        # Если нет отелей в тёплых странах, возвращаем пустой queryset
+        if not warm_hotels.exists():
+            return Hotel.objects.none()
+        # Выбираем случайный отель из списка тёплых отелей
+        selected_hotel = choice(list(warm_hotels))
+        # Фильтруем отели по стране выбранного отеля сортировка пока не согласована
+        queryset = Hotel.objects.filter(country=selected_hotel.country).order_by("?")
+        return queryset
 
     # def perform_create(self, serializer):
     #     # При создании записи устанавливаем поле created_by равным текущему пользователю
