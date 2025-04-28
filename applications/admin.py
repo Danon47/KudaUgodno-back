8d92744c251cc3e80654b5e41a13343232c38115
Tatyana Kharlamova <kharlamova2785@gmail.com>
2025-04-28 16:53:42 +0300

    [+] Добавление модели заявки на отель в административную панель

--- applications/admin.py
+++ applications/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from applications.models import Application
+from applications.models import Application, HotelApplication
 
 
 @admin.register(Application)
@@ -9,6 +9,17 @@ class ApplicationAdmin(admin.ModelAdmin):
     Админ панель для модели Application
     """
 
-    list_display = ("pk", "tour", "hotel", "room", "email", "phone_number", "status")
+    list_display = ("pk", "tour", "email", "phone_number", "status")
     list_filter = ("tour", "status")
     search_fields = ("tour", "email")
+
+
+@admin.register(HotelApplication)
+class HotelApplicationAdmin(admin.ModelAdmin):
+    """
+    Админ панель для модели HotelApplication
+    """
+
+    list_display = ("pk", "hotel", "room", "email", "phone_number", "status")
+    list_filter = ("hotel", "status")
+    search_fields = ("hotel", "email")
