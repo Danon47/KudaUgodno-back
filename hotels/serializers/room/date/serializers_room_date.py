fbe8278eb368ca9c54397c60b1ef9164f92d7fce
Никита Шидогубов <mered1an@live.com>
2025-05-16 16:21:30 +0300

    [~] Изменение отображения сериализация дат для номеров/категорий R: выводилась не та инфа FB: список всех дат для определённого отеля выводились хорошо, но неоходимо было добавить гибкости, чтобы в номере выводилась только стоимость именно этого номера в эти даты

--- hotels/serializers/room/date/serializers_room_date.py
+++ hotels/serializers/room/date/serializers_room_date.py
@@ -5,20 +5,27 @@ from hotels.models.room.date.models_room_date import RoomCategory, RoomDate
 
 class RoomCategorySerializer(serializers.ModelSerializer):
     """
-    Сериализатор категорий номеров в отеле и их стоимость
+    Сериализатор категорий номеров в отеле и их стоимость.
     """
 
     class Meta:
         model = RoomCategory
-        fields = ("id", "room", "price")
+        fields = ("price",)
 
 
-class RoomDateSerializer(serializers.ModelSerializer):
+class RoomCategoryIdSerializer(RoomCategorySerializer):
     """
-    Сериализатор дат для номеров
+    Сериализация для того, чтобы к стоимости добавить номер
     """
 
-    categories = RoomCategorySerializer(many=True)
+    class Meta(RoomCategorySerializer.Meta):
+        fields = ("room",) + RoomCategorySerializer.Meta.fields
+
+
+class RoomDateBaseSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор дат для номеров.
+    """
 
     class Meta:
         model = RoomDate
@@ -29,5 +36,40 @@ class RoomDateSerializer(serializers.ModelSerializer):
             "available_for_booking",
             "stock",
             "share_size",
-            "categories",
         )
+
+
+class RoomDateListSerializer(RoomDateBaseSerializer):
+    """
+    Сериализатор списка дат для номеров, с деталями по категориям номеров.
+    """
+
+    categories = RoomCategoryIdSerializer(many=True)
+
+    class Meta(RoomDateBaseSerializer.Meta):
+        fields = RoomDateBaseSerializer.Meta.fields + ("categories",)
+
+
+class RoomDateDetailSerializer(RoomDateBaseSerializer):
+    """
+    Детальный сериализатор RoomDate с выводом стоимости конкретного номера.
+    Требует передачи номера через context['room'].
+    """
+
+    price = serializers.SerializerMethodField()
+
+    class Meta(RoomDateBaseSerializer.Meta):
+        fields = RoomDateBaseSerializer.Meta.fields + ("price",)
+
+    def get_price(self, room_date: RoomDate) -> int | None:
+        """
+        Возвращает стоимость номера на дату.
+        Если номер не задан (например, если мы не передаем `room` в контекст сериализатора), то возвращает None.
+        """
+        room = self.context.get("room")
+        if not room:
+            return None
+        try:
+            return room_date.categories.get(room=room).price
+        except RoomCategory.DoesNotExist:
+            return None
