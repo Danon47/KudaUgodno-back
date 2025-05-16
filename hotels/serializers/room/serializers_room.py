b12588d32556e6e845de9cd79434f50861a6c71c
Никита Шидогубов <mered1an@live.com>
2025-05-16 16:09:12 +0300

    [~] Изменение отображения сериализация номера R: не были добавлены правила в отеле, и даты FB: правила для создания требуют id, а при отображении показывается название правила и его опция. Даты отображаются в определённом номере свои

--- hotels/serializers/room/serializers_room.py
+++ hotels/serializers/room/serializers_room.py
@@ -1,14 +1,18 @@
+from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 from hotels.models.room.date.models_room_date import RoomDate
 from hotels.models.room.models_room import Room
 from hotels.serializers.hotel.type_of_meals.serializers_type_of_meals import TypeOfMealSerializer
-from hotels.serializers.room.date.serializers_room_date import RoomDateSerializer
+from hotels.serializers.room.date.serializers_room_date import RoomDateDetailSerializer
 from hotels.serializers.room.photo.serializers_room_photo import RoomPhotoSerializer
+from hotels.serializers.room.rules.serializers_room_rules import RoomRulesSerializer
 
 
 class RoomBaseSerializer(serializers.ModelSerializer):
-    """Базовый сериализатор номера"""
+    """
+    Базовый сериализатор номера.
+    """
 
     class Meta:
         model = Room
@@ -26,10 +30,14 @@ class RoomBaseSerializer(serializers.ModelSerializer):
             "amenities_coffee",
             "amenities_bathroom",
             "amenities_view",
+            "rules",
         )
 
 
 class RoomDetailSerializer(RoomBaseSerializer):
+    """
+    Сериалиазатор для вывода детальной информации о номере.
+    """
 
     photo = RoomPhotoSerializer(
         source="room_photos",
@@ -38,6 +46,7 @@ class RoomDetailSerializer(RoomBaseSerializer):
     )
     date = serializers.SerializerMethodField()
     type_of_meals = TypeOfMealSerializer(many=True)
+    rules = RoomRulesSerializer(many=True)
 
     class Meta(RoomBaseSerializer.Meta):
         model = Room
@@ -46,8 +55,17 @@ class RoomDetailSerializer(RoomBaseSerializer):
             "photo",
         )
 
+    @extend_schema_field(RoomDateDetailSerializer(many=True))
     def get_date(self, obj: RoomDate) -> list:
-        """Получение всех RoomDate, связанных с этим Room через RoomCategory"""
-        # сначала идёт RoomDate, потом RoomCategory
-        room_dates = RoomDate.objects.filter(categories__room=obj).distinct()
-        return RoomDateSerializer(room_dates, many=True).data
+        """
+        Получает список объектов RoomDate связанных с номером.
+        Этот метод фильтрует объекты RoomDate, связанные с указанным номером, по его категориям, обеспечивая
+        уникальность и упорядочивая их по дате начала.
+        Затем он сериализует список с помощью RoomDateSerializer.
+        Аргументы:
+            obj (Room): Экземпляр номера, для которого извлекаются даты.
+        Returns:
+            list: Список сериализованных объектов RoomDate.
+        """
+        room_dates = RoomDate.objects.filter(categories__room=obj).distinct().order_by("start_date")
+        return RoomDateDetailSerializer(room_dates, many=True, context={"room": obj}).data
