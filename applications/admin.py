5345e6c48adf083925cfa4d12713d5eee7f379b8
Никита Шидогубов <mered1an@live.com>
2025-05-23 19:07:02 +0300

    [~] Изменение Django админки заявок R: неправильный импорт FB: правка импорта новых моделей

--- applications/admin.py
+++ applications/admin.py
@@ -1,20 +1,9 @@
 from django.contrib import admin
 
-from applications.models import Application, HotelApplication
+from applications.models import ApplicationHotel, ApplicationTour
 
 
-@admin.register(Application)
-class ApplicationAdmin(admin.ModelAdmin):
-    """
-    Админ панель для модели Application
-    """
-
-    list_display = ("pk", "tour", "email", "phone_number", "status")
-    list_filter = ("tour", "status")
-    search_fields = ("tour", "email")
-
-
-@admin.register(HotelApplication)
+@admin.register(ApplicationHotel)
 class HotelApplicationAdmin(admin.ModelAdmin):
     """
     Админ панель для модели HotelApplication
@@ -23,3 +12,14 @@ class HotelApplicationAdmin(admin.ModelAdmin):
     list_display = ("pk", "hotel", "room", "email", "phone_number", "status")
     list_filter = ("hotel", "status")
     search_fields = ("hotel", "email")
+
+
+@admin.register(ApplicationTour)
+class ApplicationAdmin(admin.ModelAdmin):
+    """
+    Админ панель для модели Application
+    """
+
+    list_display = ("pk", "tour", "email", "phone_number", "status")
+    list_filter = ("tour", "status")
+    search_fields = ("tour", "email")
