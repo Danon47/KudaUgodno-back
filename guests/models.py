585f5f21183c795f8c6235cd79c518a304ebc037
Никита Шидогубов <mered1an@live.com>
2025-05-23 17:12:23 +0300

    [~] Исправление модели R: не верно выглядят пустые поля FB: привёл к единому стандарту

--- guests/models.py
+++ guests/models.py
@@ -1,13 +1,14 @@
 from django.core.validators import RegexValidator
 from django.db import models
 
+from all_fixture.fixture_views import NULLABLE
 from users.models import User
 
 
 class PersonBase(models.Model):
     firstname = models.CharField(max_length=50, verbose_name="Имя")
     lastname = models.CharField(max_length=50, verbose_name="Фамилия")
-    surname = models.CharField(max_length=50, verbose_name="Отчество", blank=True, null=True)
+    surname = models.CharField(max_length=50, verbose_name="Отчество", **NULLABLE)
     date_born = models.DateField(verbose_name="Дата рождения", help_text="Формат: DD-MM-YYYY")
     citizenship = models.CharField(max_length=100, verbose_name="Гражданство")
 
@@ -23,25 +24,25 @@ class Guest(PersonBase):
     russian_passport_no = models.CharField(
         verbose_name="Серия/номер российского паспорта",
         help_text="Формат: XXXX XXXXXX",
-        blank=True,
-        null=True,
         validators=[
             RegexValidator(regex=r"^[0-9]{4} [0-9]{6}$", message="Введите серия/номер в формате: XXXX XXXXXX")
         ],
+        **NULLABLE,
     )
 
     international_passport_no = models.CharField(
         verbose_name="Серия/номер иностранного паспорта",
         help_text="Формат: XX XXXXXXXX",
-        blank=True,
-        null=True,
         validators=[
             RegexValidator(regex=r"^[0-9]{2} [0-9]{7}$", message="Введите серия/номер в формате: XX XXXXXXXX")
         ],
+        **NULLABLE,
     )
 
     validity_international_passport = models.DateField(
-        verbose_name="Срок действия иностранного паспорта", help_text="Формат: DD-MM-YYYY", blank=True, null=True
+        verbose_name="Срок действия иностранного паспорта",
+        help_text="Формат: DD-MM-YYYY",
+        **NULLABLE,
     )
 
     user_owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь cоздавший гостя")
