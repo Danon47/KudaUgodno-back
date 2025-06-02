6230a0fc344c187887a4a28b165a8ad20fa424c2
andre <andreyperm159@mail.ru>
2025-06-02 20:37:00 +0500

    [+]Создание сущности блог R: Добавляю сущность согласно ТЗ FB: Добавил модель категории, тег к статье блога

--- blogs/models.py
+++ blogs/models.py
@@ -1,24 +1,74 @@
 from django.db import models
+from all_fixture.fixture_views import NULLABLE
+
+
+class Category(models.Model):
+    """Модель категории для статей блога"""
+
+    name = models.CharField(max_length=50, verbose_name="Название категории", help_text="Название категории")
+    slug = models.SlugField(max_length=50, unique=True, verbose_name="Slug категории", help_text="Slug категории")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    """Модель тега для статей блога"""
+
+    name = models.CharField(max_length=50, verbose_name="Название тега", help_text="Название тега")
+    slug = models.SlugField(max_length=50, unique=True, verbose_name="Slug тега", help_text="Slug тега")
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
 
 
 class Article(models.Model):
     """Модель статья"""
 
-    title = models.CharField(max_length=100, verbose_name="Заголовок статьи", help_text="Заголовок статьи")
-    autor = models.CharField(max_length=100, verbose_name="Автор статьи")
-    content = models.TextField(verbose_name="Текст статьи")
-    pub_date = models.DateField(verbose_name="когда выложили статью")
-    short_description = models.CharField(max_length=250, verbose_name="Краткое описание, для ленты новостей")
-    is_published = models.BooleanField(default=False, verbose_name="опубликована ли статья")
-    views_count = models.PositiveIntegerField(default=0, verbose_name="счетчик просмотров")
-    rating = models.FloatField(default=0, verbose_name="оценка статьи")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
+    title = models.CharField(
+        max_length=100, verbose_name="Заголовок статьи", help_text="Заголовок статьи (максимум 100 символов)"
+    )
+    autor = models.CharField(
+        max_length=100, verbose_name="Автор статьи", help_text="Автор стать (максимум 100 символов)"
+    )
+    content = models.TextField(verbose_name="Текст статьи", help_text="Текст статьи")
+    pub_date = models.DateField(
+        verbose_name="Когда выложили статью", help_text="Когда выложили статью. Формат: ГГГГ-ММ-ДД"
+    )
+    short_description = models.CharField(
+        max_length=250,
+        verbose_name="Краткое описание, для ленты новостей",
+        help_text="Краткое описание, для ленты новостей (максимум 250 символов)",
+    )
+    is_published = models.BooleanField(
+        default=False, verbose_name="Опубликована ли статья", help_text="Опубликована ли статья"
+    )
+    views_count = models.PositiveIntegerField(
+        default=0, verbose_name="Счетчик просмотров", help_text="Счетчик просмотров"
+    )
+    rating = models.FloatField(default=0, verbose_name="Оценка статьи", help_text="Оценка статьи")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания", help_text="Дата создания")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата последнего изменения", help_text="Дата последнего изменения"
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, **NULLABLE, verbose_name="категория")
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name="Теги")
 
     class Meta:
         verbose_name = "Статья"
         verbose_name_plural = "Статьи"
-        ordering = ["-created_at", "-created_at"]
+        ordering = ["-pub_date", "-created_at"]
 
     def __str__(self):
         return self.title
@@ -32,7 +82,9 @@ class ArticleImage(models.Model):
     order = models.PositiveIntegerField(default=0)  # Поле для указания порядка отображения изображений в статье
 
     class Meta:
+        verbose_name = "Изображение статьи"
+        verbose_name__plural = "Изображения для статьи"
         ordering = ["order"]
 
     def __str__(self):
-        return f"Image for {self.article.title}"
+        return f"Изображение для {self.article.title}"
