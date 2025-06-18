8b7b1d091025a82079e49898d0343205d54dc9d2
Никита Шидогубов <mered1an@live.com>
2025-06-18 14:46:50 +0300

    [+] добавление другой модели для календаря R: хочу найти лучшее решение из двух, и потом не нужное удалить FB:

--- rooms/views.py
+++ rooms/views.py
@@ -3,7 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import OpenApiResponse, extend_schema, extend_schema_view
 from rest_framework import viewsets
 
-from all_fixture.fixture_views import (  # room_date_id,; room_date_settings,
+from all_fixture.fixture_views import (
     hotel_id,
     id_room,
     limit,
@@ -102,74 +102,6 @@ class RoomViewSet(viewsets.ModelViewSet):
         hotel = get_object_or_404(Hotel, id=self.kwargs["hotel_id"])
         # Сохраняем Room с привязкой к Hotel
         serializer.save(hotel=hotel)
-
-
-# @extend_schema_view(
-#     list=extend_schema(
-#         summary="Список номеров",
-#         description="Получение списка всех номеров с пагинацией",
-#         parameters=[limit, offset, hotel_id],
-#         responses={
-#             200: RoomDateListSerializer(many=True),
-#             400: OpenApiResponse(description="Ошибка запроса"),
-#         },
-#         tags=[room_date_settings["name"]],
-#     ),
-#     create=extend_schema(
-#         summary="Добавление номера",
-#         description="Создание нового номера",
-#         request=RoomDateListSerializer,
-#         parameters=[hotel_id],
-#         responses={
-#             201: RoomDateListSerializer,
-#             400: OpenApiResponse(description="Ошибка запроса"),
-#         },
-#         tags=[room_date_settings["name"]],
-#     ),
-#     retrieve=extend_schema(
-#         summary="Детали номера",
-#         description="Получение информации о номере",
-#         parameters=[hotel_id, room_date_id],
-#         responses={
-#             200: RoomDateListSerializer,
-#             404: OpenApiResponse(description="Ошибка запроса"),
-#         },
-#         tags=[room_date_settings["name"]],
-#     ),
-#     update=extend_schema(
-#         summary="Полное обновление номера",
-#         description="Обновление всех полей номера",
-#         request=RoomDateListSerializer,
-#         parameters=[hotel_id, room_date_id],
-#         responses={
-#             200: RoomDateListSerializer,
-#             400: OpenApiResponse(description="Ошибка запроса"),
-#             404: OpenApiResponse(description="Номер не найден"),
-#         },
-#         tags=[room_date_settings["name"]],
-#     ),
-#     destroy=extend_schema(
-#         summary="Удаление номера",
-#         description="Полное удаление номера",
-#         parameters=[hotel_id, room_date_id],
-#         responses={
-#             204: OpenApiResponse(description="Номер удален"),
-#             404: OpenApiResponse(description="Номер не найден"),
-#         },
-#         tags=[room_date_settings["name"]],
-#     ),
-# )
-# class RoomDateViewSet(viewsets.ModelViewSet):
-#     queryset = RoomDate.objects.none()
-#     pagination_class = CustomLOPagination
-#     serializer_class = RoomDateListSerializer
-#
-#     def get_queryset(self):
-#         # Получаем ID отеля из URL параметров
-#         hotel_id = self.kwargs["hotel_id"]
-#
-#         # Фильтруем Room по связи с Hotel
-#         return RoomDate.objects.filter(categories__room__hotel_id=hotel_id).distinct()
 
 
 @extend_schema_view(
