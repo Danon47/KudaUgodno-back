9599970fc81dca7890f36fa5c89b8e08088186b4
Никита Шидогубов <mered1an@live.com>
2025-06-27 11:33:17 +0300

    [*] Изменение ручки check_token R: фронту нужна ручка, к которой можно обратиться и получить сразу всю инфу по пользователю FB: переписал ответ и переименовал ручку

--- users/views.py
+++ users/views.py
@@ -528,10 +528,12 @@ class AuthViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
             ),
         },
     )
-    @action(detail=False, methods=["get"], url_path="check-token", permission_classes=[IsAuthenticated])
-    def check_token(self, request):
-        """Возвращает 200 OK, если токен валиден, иначе 401."""
-        return Response({"message": "Токен активен"}, status=status.HTTP_200_OK)
+    @action(detail=False, methods=["get"], url_path="fetch_me", permission_classes=[IsAuthenticated])
+    def fetch_me(self, request):
+        """Проверяет токен и возвращает данные пользователя через сериализатор."""
+        user = request.user
+        serializer = UserSerializer(user, context={"request": request})
+        return Response({"message": "Токен активен", "user": serializer.data}, status=status.HTTP_200_OK)
 
     @extend_schema(
         summary="Обновление токенов",
