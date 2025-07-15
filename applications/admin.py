557140d83de0809f37643d0d9f3a6d9bf144ac97
Никита Шидогубов <mered1an@live.com>
2025-07-15 13:44:46 +0300

    [*] Обновление заявок/бронирований согласно новому макету, приведение к единому стандарту

--- applications/admin.py
+++ applications/admin.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 
 from applications.models import (
-    ApplicationCancellationInsurance,
     ApplicationHotel,
-    ApplicationMedicalInsurance,
     ApplicationTour,
-    ApplicationVisa,
 )
 
 
 @admin.register(ApplicationHotel)
-class HotelApplicationAdmin(admin.ModelAdmin):
+class ApplicationHotelAdmin(admin.ModelAdmin):
     """
     Админ панель для модели HotelApplication
     """
@@ -21,7 +18,7 @@ class HotelApplicationAdmin(admin.ModelAdmin):
 
 
 @admin.register(ApplicationTour)
-class ApplicationAdmin(admin.ModelAdmin):
+class ApplicationTourAdmin(admin.ModelAdmin):
     """
     Админ панель для модели Application
     """
@@ -29,43 +26,3 @@ class ApplicationAdmin(admin.ModelAdmin):
     list_display = ("pk", "tour", "email", "phone_number", "status")
     list_filter = ("tour", "status")
     search_fields = ("tour", "email")
-
-
-@admin.register(ApplicationVisa)
-class VisaApplicationAdmin(admin.ModelAdmin):
-    """
-    Админ панель для модели VisaApplication
-    """
-
-    list_display = (
-        "id",
-        "count",
-        "price",
-        "total_price",
-    )
-
-
-@admin.register(ApplicationMedicalInsurance)
-class MedicalInsuranceApplicationAdmin(admin.ModelAdmin):
-    """
-    Админ панель для модели ApplicationMedicalInsurance
-    """
-
-    list_display = (
-        "id",
-        "count",
-        "price",
-        "total_price",
-    )
-
-
-@admin.register(ApplicationCancellationInsurance)
-class CancellationInsuranceApplicationAdmin(admin.ModelAdmin):
-    """
-    Админ панель для модели ApplicationCancellationInsurance
-    """
-
-    list_display = (
-        "id",
-        "total_price",
-    )
