b24db92bb90e995338a84743ca5776c4ca948919
Annabse27 <annabse27@gmail.com>
2025-09-09 12:20:25 +0200

    [~]: уточнение синтаксисв вьюшке акционных отелей с новыми правками
    R:
    
    FB:

--- hotels/views.py
+++ hotels/views.py
@@ -257,18 +257,15 @@ class HotelsHotView(viewsets.ModelViewSet):
 
     def get_queryset(self):
         """Получение запроса с отелями по одному из каждой страны с минимальной ценой."""
-        today = timezone.now().date()
-
-        min_price_subquery = (
+        total_price_subquery = (
             CalendarPrice.objects.filter(
                 calendar_date__discount=True,
                 calendar_date__available_for_booking=True,
-                calendar_date__start_date__gte=today,
                 room__hotel=OuterRef("pk"),
                 price__isnull=False,
             )
             .annotate(
-                price_with_discount=Case(
+                total_price_with_discount=Case(
                     When(
                         calendar_date__discount_amount__gt=1,
                         then=F("price") - F("calendar_date__discount_amount"),
@@ -277,33 +274,32 @@ class HotelsHotView(viewsets.ModelViewSet):
                         calendar_date__discount_amount__gt=0,
                         then=F("price") * (1 - F("calendar_date__discount_amount")),
                     ),
-                    default=F("price"),
                     output_field=DecimalField(max_digits=10, decimal_places=2),
                 )
             )
-            .order_by("price_with_discount", "price")
-            .values("price", "price_with_discount")[:1]
+            .order_by("total_price_with_discount", "price")
+            .exclude(total_price_with_discount=None)
+            .values("price", "total_price_with_discount")[:1]
         )
 
         queryset = (
             Hotel.objects.filter(is_active=True)
             .prefetch_related("hotel_photos")
             .annotate(
-                min_price_without_discount=Subquery(
-                    min_price_subquery.values("price"),
+                total_price_without_discount=Subquery(
+                    total_price_subquery.values("price"),
                     output_field=DecimalField(max_digits=10, decimal_places=2),
                 ),
-                min_price_with_discount=Subquery(
-                    min_price_subquery.values("price_with_discount"),
+                total_price_with_discount=Subquery(
+                    total_price_subquery.values("total_price_with_discount"),
                     output_field=DecimalField(max_digits=10, decimal_places=2),
                 ),
             )
-            .exclude(min_price_with_discount=None)  # чтобы страны без акций не попадали
             .annotate(
                 grouped_countries=Window(
                     expression=RowNumber(),
                     partition_by=[F("country")],
-                    order_by=F("min_price_with_discount").asc(),
+                    order_by=F("total_price_with_discount").asc(),
                 )
             )
             .filter(grouped_countries=1)
